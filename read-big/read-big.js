8bd4e03bcbefaf521eb598dd318fd9d727782c39
Jeevan Vaishnav <jeevanvaishnavv@gmail.com>
2024-10-10 00:54:02 +0530

    Implemented even num in big data

--- read-big/read-big.js
+++ read-big/read-big.js
@@ -8,15 +8,32 @@ const fs = require("node:fs/promises");
   });
   const streamWrite = fileHandleWrite.createWriteStream();
 
+  let split = ''
   streamRead.on("data", (chunk) => {
-    if (!streamWrite.write(chunk)) {
-      console.log("streamRead.Pause");
-      streamRead.pause();
+    const numbers = chunk.toString('utf-8').split("  "); // each chunk is converted to string and each character is 8bits
+
+    if(Number(numbers[0]) !== Number(numbers[1]) - 1){
+        if(split) numbers[0] = split.trim() + numbers[0].trim();
     }
+
+    // console.log(Number(numbers[numbers.length - 2] + 1), Number(numbers[numbers.length - 1]));
+    if(Number(numbers[numbers.length - 2]) + 1 !== Number(numbers[numbers.length - 1])){
+      split = numbers.pop();
+    }
+
+    numbers.forEach((number)=>{
+      let n = Number(number);
+      if(n % 2 === 0){
+        if (!streamWrite.write(" " + n + " ")) {
+          streamRead.pause();
+        }
+      }
+    })
+
+    
   });
 
   streamWrite.on("drain", () => {
-    console.log("Drained");
     streamRead.resume();
   });
 })();

b31c683096db3d3eb153d3fa8ee217c95bd39950
Jeevan Vaishnav <jeevanvaishnavv@gmail.com>
2024-10-14 17:22:11 +0530

    copy code pushed

--- copy/copy.js
+++ copy/copy.js
@@ -1,2 +1,11 @@
-// 03:02:43
-console.log("copy stream..")
+const fs = require('node:fs/promises');
+
+(async () =>{
+    const destFile = await fs.open('dest.txt', 'w')
+
+    const result = await fs.readFile('big-gigantic.txt');
+
+    console.log(result)
+    
+    await destFile.write(result);
+})();

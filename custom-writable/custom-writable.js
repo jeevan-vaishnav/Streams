7172d98ecef2689e54a7724b682c063c9e1adc2b
Jeevan Vaishnav <jeevanvaishnavv@gmail.com>
2024-10-21 22:14:25 +0530

    custom writable

--- custom-writable/custom-writable.js
+++ custom-writable/custom-writable.js
@@ -5,43 +5,82 @@ class FileWritableStream extends Writable {
     super({ highWaterMark });
     this.fileName = fileName;
     this.fd = null;
+    this.chunks = [];
+    this.chunkSize = 0;
+    this.numberOfWrites = 0;
   }
 
+  //This will run after the constructor, and it will put off all calling the other 
+  //methods until we call the callback function 
   _construct(callback) {
     console.log("Construct");
     fs.open(this.fileName, "w", (err, fd) => {
       if (err) {
+        //so if we have call the callback with an argument, it means that we have an error
+        //and we should not proceed 
         console.log("err");
         callback(err);
       } else {
-        console.log('no err')
+        console.log('no err! file opened' )
         this.fd = fd;
+        //no argument means it was successful
         callback();  
     }
     });
   }
 
   _write(chunk, encoding, callback) {
+    this.chunks.push(chunk);
+    this.chunkSize += chunk.length;
     // do our write operation..
-    console.log(chunk.toString());
+    console.log('chunk size',this.chunkSize)
+    console.log('writable high watermark',this.writableHighWaterMark)
+    if(this.chunkSize > this.writableHighWaterMark){
+      fs.write(this.fd,Buffer.concat(this.chunks),0,this.chunkSize,(err)=>{
+        if(err){
+          return callback(err);
+        }
+        this.chunks = [];
+        this.chunkSize = 0;
+        ++this.numberOfWrites;
+        callback();
+      })
+    }else{
     //when we're done, we should call the callback function
-    callback();
+      callback();
+    }
+    console.log("Write", this.numberOfWrites);
   }
 
   _final(callback) {
     console.log("Final");
+    fs.write(this.fd,Buffer.concat(this.chunks),(err)=>{
+      if(err){
+        return callback(err)
+      }
+      this.chunks = [];
+      callback();
+    })
     callback();
   }
 
   _destroy(error, callback) {
-    console.log("Destroy");
-    callback();
+    console.log("Number of writes", this.numberOfWrites);
+    if(this.fd){
+      fs.close(this.fd,(err)=>{
+        callback(error | err)
+      })
+    }else{
+      callback(error);
+    }
+  
+    
   }
 }
 
 const stream = new FileWritableStream({ highWaterMark: 1800,fileName: "test.txt" });
-stream.write(Buffer.from("This is some string"));
-stream.end(Buffer.from("Our last write"));
+stream.write(Buffer.from("This"));
+stream.end(Buffer.from("Our"));
 
 stream.on("drain", () => {
   console.log("Drained");
